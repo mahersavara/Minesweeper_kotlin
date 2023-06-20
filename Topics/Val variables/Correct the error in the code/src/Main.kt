9e0a94635e6b316d68e9295877ad92f6579d6c23
kdlinh <kdlinh8811@gmail.com>
2023-06-20 15:48:23 +0700

    Beginning of Targeting!

---
+++ Topics/Val variables/Correct the error in the code/src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+     val CONST: Int
+    var listNumber = mutableListOf<Int>()
+    CONST = 100
+    listNumber.add(CONST * CONST)
+    listNumber.add(CONST * CONST * CONST)
+    listNumber.add(CONST * CONST * CONST * CONST)
+    println(listNumber)
+    val text: String = "hello, world"
+    val number:Int = 3
+    var gree: Int
+// -> cần có type cho gree !
+}
